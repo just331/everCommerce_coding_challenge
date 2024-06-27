a1321f708367b14e81ebdbf5fc85705f9697eb5d
Justin <justinr331@gmail.com>
2024-06-27 12:01:14 -0500

    adding test cases

--- tests/test_prime_generator.py
+++ tests/test_prime_generator.py
@@ -2,6 +2,7 @@ import unittest
 from utils import is_prime
 from prime_generator import prime_generator
 
+
 class TestIsPrime(unittest.TestCase):
     def test_is_prime(self):
         self.assertFalse(is_prime(0))
@@ -10,10 +11,21 @@ class TestIsPrime(unittest.TestCase):
         self.assertTrue(is_prime(3))
         self.assertFalse(is_prime(4))
         self.assertTrue(is_prime(5))
+        self.assertFalse(is_prime(7900))
         self.assertTrue(is_prime(7919))
         self.assertFalse(is_prime(7920))
+
 
 class TestPrimeGenerator(unittest.TestCase):
     def test_prime_generator(self):
         # general case for finding primes in range of nums
-        self.assertEqual(prime_generator(1, 10), [2,3,5,7])
+        self.assertEqual(prime_generator(1, 10), [2, 3, 5, 7])
+        # inverse range general case
+        self.assertEqual(prime_generator(10, 1), [2, 3, 5, 7])
+        # case for finding primes in range of 7900 - 7920
+        self.assertEqual(prime_generator(7900, 7920), [7901, 7907, 7919])
+        # inverse range case for above range
+        self.assertEqual(prime_generator(7900, 7920), [7901, 7907, 7919])
+        # primes between 1 - 100
+        self.assertEqual(prime_generator(1, 100), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+                                                   41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ def is_prime(value:int) -> bool:
         return False
 
     # find if num not in base cases is prime
-    for i in range(3, int(value ** 2) + 1, 2):
+    for i in range(3, int(value ** .5) + 1, 2):
         if value % i == 0:
             return False
     return True
