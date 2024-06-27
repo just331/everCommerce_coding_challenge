db221e13ffc132b1fc55d70b013f56b9bfab1955
Justin <justinr331@gmail.com>
2024-06-27 11:43:37 -0500

    creating main to handle cmd line input

--- utils.py
+++ utils.py
@@ -9,6 +9,7 @@ def is_prime(value:int) -> bool:
     if value % 2 == 0:
         return False
 
+    # find if num not in base cases is prime
     for i in range(3, int(value ** 2) + 1, 2):
         if value % i == 0:
             return False
